5a5a21239f770851c149815aaa9cbe1f3950f26f
nandkumar birgad <healthcare@example.com>
2025-11-15 17:42:03 +0530

    commit again for tiDB test

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, jsonify, request
 import decimal
 import json
 from datetime import date, datetime
-import ssl
 
 # ---------- JSON Helper ----------
 class DecimalEncoder(json.JSONEncoder):
@@ -17,18 +16,17 @@ class DecimalEncoder(json.JSONEncoder):
 # ---------- Flask App ----------
 app = Flask(__name__, template_folder='template', static_folder='static')
 
-# ---------- MySQL Connection (PyMySQL ONLY) ----------
+# ---------- MySQL Connection (WITH SSL for TiDB Cloud) ----------
 def get_db_connection():
-    ssl_context = ssl.create_default_context()
     return pymysql.connect(
         host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
         user='4MtRB8TtYgRCw9d.root',
-        port=int('4000'),
+        port=4000,
         password='PTcrp6v3lYMq6hJd',
         database='test',
-        # ADD THESE LINES TO ENABLE SSL CONNECTION
-        ssl_disabled=False,
-        ssl_context=ssl_context
+        cursorclass=pymysql.cursors.Cursor,
+        ssl={"fake_flag_to_enable_tls": True},
+        ssl_verify_cert=False
     )
 
 # ---------- ROUTES ----------
